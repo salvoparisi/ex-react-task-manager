2d0550c2662abacb6fd3aa606843bc1c0d823eb4
salvoparisi <psalvo065@gmail.com>
2025-03-12 22:58:52 +0100

    milestone 2

--- frontend/src/pages/TaskList.jsx
+++ frontend/src/pages/TaskList.jsx
@@ -1,12 +1,16 @@
-import { useEffect, useContext } from "react"
+import { useEffect, useContext, useState } from "react"
 import ContextApi from "../Context/ContextApi";
 
 function TaskList() {
     const apiUrl = useContext(ContextApi)
-    useEffect(() => {
-        console.log(apiUrl);
+    const [tasks, setTasks] = useState([])
 
-    }, [])
+    useEffect(() => {
+        fetch(`${apiUrl}/tasks`)
+            .then((res) => res.json())
+            .then((data) => setTasks(data))
+    })
+
     return (
         <div>tasklist</div>
     )
