be5db3ab422a86b646717f5fc8ba35a385eefaab
salvoparisi <psalvo065@gmail.com>
2025-03-12 22:41:58 +0100

    fix .env

--- frontend/src/pages/TaskList.jsx
+++ frontend/src/pages/TaskList.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react"
 function TaskList() {
     const apiUrl = import.meta.env.VITE_API_URL
     useEffect(() => {
-        console.log(import.meta.env);
+        console.log(apiUrl);
 
     }, [])
     return (
