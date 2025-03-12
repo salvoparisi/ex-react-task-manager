fd5965f8bd801fe9ff2b84bc926bc86b78bb50db
salvoparisi <psalvo065@gmail.com>
2025-03-12 22:32:22 +0100

    add .env

--- frontend/src/pages/TaskList.jsx
+++ frontend/src/pages/TaskList.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react"
+
 function TaskList() {
+    const apiUrl = import.meta.env.VITE_API_URL
+    useEffect(() => {
+        console.log(import.meta.env);
+
+    }, [])
     return (
         <div>tasklist</div>
     )
