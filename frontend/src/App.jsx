cc95a29e9bea2f70aa075266e9388fd91752e2a2
salvoparisi <psalvo065@gmail.com>
2025-03-12 22:12:55 +0100

    milestone 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import AddTask from "./pages/AddTask.jsx"
 import TaskList from "./pages/TaskList.jsx"
+import Navbar from "./Layout/Navbar.jsx"
 import './App.css'
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/addtask" element={<AddTask />} />
         <Route path="/tasklist" element={<TaskList />} />
---
+++ frontend/src/Layout/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+function Navbar() {
+    return (
+        <div className='navBar'>
+            <div className='addtask'>
+                <Link to="/addtask">Add Task</Link>
+            </div>
+            <div className='tasklist'>
+                <Link to="/tasklist">Task List</Link>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
