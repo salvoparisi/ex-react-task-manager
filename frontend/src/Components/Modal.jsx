92117a59cb89d3a204b6a10041e00e3f09122e8a
salvoparisi <psalvo065@gmail.com>
2025-03-18 00:49:12 +0100

    add createPortal, mileston 9

--- frontend/src/Components/Modal.jsx
+++ frontend/src/Components/Modal.jsx
@@ -1,12 +1,14 @@
+import { createPortal } from "react-dom"
 function Modal({ show, title, content, onClose, onConfirm }) {
 
-    return show && (
+    return show && createPortal(
         <div>
             <h2>{title}</h2>
             <p>{content}</p>
             <button onClick={onClose}>Annulla</button>
             <button onClick={onConfirm}>Conferma</button>
-        </div>
+        </div>,
+        document.body
     )
 }
 
